1af03981f572fa09d97556c4645e24ece016c48c
Guilherme Santiago <guilherme.santiago95@gmail.com>
2019-02-18 15:08:23 -0300

    Add VerticalSpacer component

---
+++ src/components/VerticalSpacer/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+
+function VerticalSpacer (props) {
+  return <Spacer {...props} />
+}
+
+const Spacer = styled.div`
+  > * + * {
+    margin-top: ${p => p.space}px !important;
+  }
+`
+
+VerticalSpacer.propTypes = {
+  /** Spacing between the children */
+  space: PropTypes.number
+}
+
+VerticalSpacer.defaultProps = {
+  space: 25
+}
+
+export default VerticalSpacer
