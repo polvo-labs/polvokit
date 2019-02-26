31d0c08b2c1eef60ffe2b001d3f7c68d8dd9ec08
Guilherme Santiago <guilherme.santiago95@gmail.com>
2019-02-25 22:29:50 -0300

    add docs

---
+++ .docz/app/imports.js
@@ -0,0 +1,36 @@
+export const imports = {
+  'src/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'),
+  'src/components/BackButton/BackButton.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-back-button-back-button" */ 'src/components/BackButton/BackButton.mdx'),
+  'src/components/Badge/Badge.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-badge-badge" */ 'src/components/Badge/Badge.mdx'),
+  'src/components/Button/Button.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-button-button" */ 'src/components/Button/Button.mdx'),
+  'src/components/Calendar/Calendar.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-calendar-calendar" */ 'src/components/Calendar/Calendar.mdx'),
+  'src/components/DashboardLayout/DashboardLayout.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-dashboard-layout-dashboard-layout" */ 'src/components/DashboardLayout/DashboardLayout.mdx'),
+  'src/components/FormGrid/FormGrid.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-form-grid-form-grid" */ 'src/components/FormGrid/FormGrid.mdx'),
+  'src/components/FormGroup/FormGroup.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-form-group-form-group" */ 'src/components/FormGroup/FormGroup.mdx'),
+  'src/components/Icon/Icon.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-icon-icon" */ 'src/components/Icon/Icon.mdx'),
+  'src/components/Image/Image.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-image-image" */ 'src/components/Image/Image.mdx'),
+  'src/components/Input/Input.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-input-input" */ 'src/components/Input/Input.mdx'),
+  'src/components/InputDatePicker/InputDatePicker.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-input-date-picker-input-date-picker" */ 'src/components/InputDatePicker/InputDatePicker.mdx'),
+  'src/components/Pagination/Pagination.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-pagination-pagination" */ 'src/components/Pagination/Pagination.mdx'),
+  'src/components/PolvoLogo/PolvoLogo.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-polvo-logo-polvo-logo" */ 'src/components/PolvoLogo/PolvoLogo.mdx'),
+  'src/components/Select/Select.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-select-select" */ 'src/components/Select/Select.mdx'),
+  'src/components/Textarea/Textarea.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-textarea-textarea" */ 'src/components/Textarea/Textarea.mdx'),
+  'src/components/VerticalSpacer/VerticalSpacer.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-vertical-spacer-vertical-spacer" */ 'src/components/VerticalSpacer/VerticalSpacer.mdx'),
+}
